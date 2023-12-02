18594d6d6ec2e142fd1d19bc642738946f82e81a
William Luke <williamluke@me.com>
2023-12-02 11:15:07 -0600

    add dependency tag

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
  platforms: [.macOS(.v10_15), .iOS(.v13)],
  products: [.library(name: "Shell", targets: ["Shell"])],
  dependencies: [
-  .package(url: "https://github.com/codeAcrylic/core.git", branch: "main"),
+  .package(url: "https://github.com/codeAcrylic/core.git", from: "0.1.0"),
   .package(url: "https://github.com/mxcl/Chalk.git", from: "0.5.0")
  ],
  targets: [
