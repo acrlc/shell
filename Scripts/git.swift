6b5393735b5adeded8711aa2803f9d971f3432f7
William Luke <williamluke@me.com>
2023-12-03 21:51:34 -0600

    add trash and git to scripts

---
+++ Scripts/git.swift
@@ -0,0 +1,171 @@
+#!/usr/bin/env swift-shell
+import Shell // ..
+
+/**
+ # git
+ an overlay for the git command that introduces *simplification*
+ #### Features
+ - converts user/repo to github addresses
+ - adds subcommand `initialize` to initialize a source with a remote url
+ - adds subcommand `source` to add a remote to an initialized repository
+ #### Usage
+ ```sh
+ # clone using shorthand user/repo
+ git clone <user/repo>
+ # clone with shorthand based on folder, assuming it's the user's name
+ git clone <repo>
+ examples:
+  git clone user/repo
+  git clone repo
+ subcommands: (will prompt by default if some or all inputs are missing)
+ # initialize a repository with a remote url
+ git initialize <option> <repository> <branch>
+ option:
+  -m, --message <input>: initial commit message (default is 'initial')
+ example: git initialize -m "starting" user/repo default
+ # source a remote using a local repository and branch
+ git source <repository> <branch>
+ example: git source user/repo default
+ ```
+ #### Caveats
+ - using `clone` assumes the domain github when using the shorthand parameter
+ - could support `clone` option or further inference to find a domain name
+ - subcommands exit but don't resolve issues with `.git` and `.gitignore`
+ */
+@main enum Git {
+ static let folder = Folder.current
+
+ static func main() {
+  var arguments = CommandLine.arguments[1...].map { $0 }
+  let command = arguments.first
+
+  do {
+   if command == "clone" {
+    // TODO: make safe for complex arguments within this scope
+    // and read parent folders names such as 'github' or 'huggingface' to
+    // so they can be used for the shorthand domain
+    if !arguments.contains(where: { $0.matches(regex: \.url) }) {
+     let path = arguments[1]
+     // note: it's possible to assume the domain based on the previous path
+     // replace with shorthand url
+     // TODO: isolate paths which could be limited here
+     if path.contains("/") {
+      arguments[1] = "https://github.com/\(path)"
+     } else {
+      // replace with folder based url
+      let user = Folder.current.name
+      arguments[1] = "https://github.com/\(user)/\(path)"
+     }
+    }
+   } else if command == "initialize" {
+    guard !folder.containsSubfolder(named: ".git") else {
+     print("current folder appears to be a repository")
+     print(
+      "delete .git or use 'git source' to add a remote to your local repo"
+     )
+     exit(2)
+    }
+
+    assertIgnored()
+    arguments.removeFirst()
+
+    var message: String?
+    if let first = arguments.first, first.hasPrefix("-") {
+     let option = first.drop(while: { $0 == "-" })
+     if option == "m" || option == "message" {
+      arguments.removeFirst()
+      guard let input = arguments.first?.wrapped else {
+       exit(1, "option -m, --message must include an input")
+      }
+      message = input
+      arguments.removeFirst()
+     } else {
+      exit(2, "unknown flag at \(first)")
+     }
+    } else {
+     print("\nEnter initial commit message(default, initial): ", terminator: .empty)
+     message = readLine()?.wrapped
+    }
+
+    try process(.git, with: "init")
+    try process(.git, with: "add", "--all")
+    try process(.git, with: "commit", "-m", message ?? "initial")
+
+    let remote = getRemote(arguments.first)
+    let branch = arguments.count > 1 ? arguments[1] : getBranch() ?? "main"
+
+    try process(.git, with: "branch", "-M", branch)
+    try process(.git, with: "remote", "add", "origin", remote)
+
+    request()
+    try process(.git, with: "push", "-u", "origin", branch)
+
+    exit(0)
+   } else if command == "source" {
+    guard folder.containsSubfolder(named: ".git") else {
+     print("current folder isn't a repository")
+     print("use 'git init && git source' or 'git initialize' to add remote")
+     exit(1)
+    }
+
+    assertIgnored()
+    arguments.removeFirst()
+
+    let remote = getRemote(arguments.first)
+    let branch = arguments.count > 1 ? arguments[1] : getBranch() ?? "main"
+
+    try process(.git, with: "remote", "add", "origin", remote)
+    try process(.git, with: "branch", "-M", branch)
+
+    request()
+    try process(.git, with: "push", "-u", "origin", branch)
+
+    exit(0)
+   }
+
+   try process(.git, with: arguments)
+  }
+  catch let error as _POSIXError { exit(error.status) }
+  catch { fatalError() }
+ }
+}
+
+extension Git {
+ static func assertIgnored() {
+  guard folder.containsFile(named: ".gitignore") else {
+   exit(2, "add a .gitignore file to this current folder to start")
+  }
+ }
+
+ static func getRemote(_ optional: String?) -> String {
+  var remote: String! = optional
+  var retry = false
+  while remote == nil {
+   print("\rEnter remote user/repo or address: ", terminator: .empty)
+
+   if let input = readLine()?.wrapped { remote = input }
+   else {
+    Shell.clearScrollback()
+    if !retry { print("Please, try again", terminator: "\n"); retry = true }
+   }
+  }
+
+  return remote.matches(regex: \.url) ? remote : "https://github.com/\(remote!)"
+ }
+
+ static func getBranch() -> String? {
+  print("\rEnter optional branch (default, main): ", terminator: .empty)
+  return readLine()?.wrapped
+ }
+
+ static func request() {
+  print("\rPush this commit to remote? [Y/n]: ", terminator: .empty)
+  if let input = readLine()?.lowercased() {
+   switch input {
+   case "y", "yes": break
+   case "n", "no": fallthrough
+   default: exit(0)
+   }
+  }
+ }
+}
---
+++ Scripts/trash.swift
@@ -0,0 +1,111 @@
+#!/usr/bin/env swift-shell
+import Shell // ..
+
+/// Sends files to the trash using Finder
+/// `trash <f, force> <[path]>`
+/// # Caveats
+/// - will not prompt when using the `force` flag
+@main enum Trash {
+ /// Optional flag to permanently delete files
+ static var force: Bool = false
+ /// Files to trash or delete
+ static var inputs: [String] = CommandLine.arguments[1...].map { $0 }
+
+ static func main() {
+  // macOS only
+  if #available(macOS 11, *) {
+   parse()
+   guard self.inputs.notEmpty else {
+    print("input <\("path", style: .boldDim)> required")
+    exit(1)
+   }
+
+   do {
+    if self.force {
+     try process(.rm, with: ["-frd"] + self.inputs)
+    } else {
+     let urls = try inputs.compactMap {
+      let url = URL(fileURLWithPath: $0)
+      do {
+       if try url.checkResourceIsReachable() { return url }
+      } catch {
+       throw Error.fileIsMissing(url)
+      }
+      return nil
+     }
+
+     /// adapted from https://github.com/aerobounce/trash.swift
+     let target = NSAppleEventDescriptor(bundleIdentifier: "com.apple.finder")
+     let event = NSAppleEventDescriptor(
+      eventClass: kAECoreSuite,
+      eventID: AEEventID(kAEDelete),
+      targetDescriptor: target,
+      returnID: AEReturnID(kAutoGenerateReturnID),
+      transactionID: AETransactionID(kAnyTransactionID)
+     )
+
+     let listDescriptor = NSAppleEventDescriptor(listDescriptor: ())
+     for (offset, url) in urls.enumerated() {
+      /// UTF-8 encoded full path with native path separators
+      let nativePath = try NSAppleEventDescriptor(
+       descriptorType: typeFileURL,
+       data: url.absoluteString.data(using: .utf8)
+      ).throwing()
+
+      // note: must add an additional offset to the list
+      listDescriptor.insert(nativePath, at: offset + 1)
+     }
+
+     event.setParam(listDescriptor, forKeyword: keyDirectObject)
+
+     do {
+      try event.sendEvent(
+       options: .noReply,
+       timeout: TimeInterval(kAEDefaultTimeout)
+      )
+     } catch let error as NSError {
+      if case -600 = error.code {
+       throw Error.finderNotRunning
+      } else {
+       throw error
+      }
+     }
+    }
+   } catch {
+    exit(error)
+   }
+  } else {
+   let platform = try! output("uname")
+   exit(2, "platform \(platform) not supported")
+  }
+ }
+}
+
+extension Trash {
+ static func parse() {
+  if let first = inputs.first, first.hasPrefix("-") {
+   let option = first.drop(while: { $0 == "-" })
+   if option == "f" || option == "force" {
+    force = true
+    inputs.removeFirst()
+   }
+  }
+ }
+
+ enum Error: LocalizedError, CustomStringConvertible {
+  case finderNotRunning, fileIsMissing(URL)
+  var _code: Int {
+   switch self {
+   case .finderNotRunning: 1
+   case .fileIsMissing: 2
+   }
+  }
+
+  var errorDescription: String? {
+   switch self {
+   case .finderNotRunning: "finder isn't running"
+   case .fileIsMissing(let url): "path missing for '\(url.relativePath)' "
+   }
+  }
+ }
+}
